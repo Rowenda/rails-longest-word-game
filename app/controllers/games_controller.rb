c12b4b573137721491676d90dda3502d9a8b59ef
Rowenda <badbat_69@hotmail.com>
2023-11-25 15:50:33 +0100

    mise a jour du code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,30 @@
-require 'open-uri'
+require "open-uri"
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = ('a'..'z').to_a
-    @selected_letters = []
-    10.times do
-      @selected_letters << @letters.sample(1)[0]
-    end
-    @selected_letters
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
-    @check_letter = 0
-    @word = params[:word]
-    @selected_letters = params[:selected_letters]
-    @selected_letters.split(' ').each { |char| @check_letter += 1 if @word.include? char }
-    @result = true if @check_letter == @word.length
-    if @result
-      url = `https://wagon-dictionary.herokuapp.com/#{@word}`
-      @response = JSON.parse(open(url).read)
-    end
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
